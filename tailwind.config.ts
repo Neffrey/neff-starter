d0fe24f3bfc521842292640b0203c8846ff12945
Neffrey <neffreyl@gmail.com>
2023-10-12 14:52:24 -0500

    Remove commented out old code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,6 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    // "./pages/**/*.{ts,tsx}",
-    // "./components/**/*.{ts,tsx}",
-    // "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
